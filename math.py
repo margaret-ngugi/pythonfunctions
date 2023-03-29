20b1c4c948cd238c9751cf1eae387f692c0286d4
margaret-ngugi <ngugiwairimumargaret@gmail.com>
2023-03-29 19:38:31 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer = a+b
+    return answer
+
+def subtract(x,y):
+    answer = x-y 
+    return answer 
+
+def divide(e,f):
+    answer = e/f
+    return answer
+def multiply(y,z):
+    answer = y*z
+    return answer
+
+def remainder(u,c):
+    answer = u%c
+    return answer        
+
